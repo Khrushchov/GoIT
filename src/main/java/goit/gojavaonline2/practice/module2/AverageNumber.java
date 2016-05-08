8aba038338cd99ee317c0162cbd12bbbe6a1013b
Copypaster <khruschiov@gmail.com>
2016-05-08 10:38:12 +0300

    module2: add task 3 -  average number

---
+++ src/main/java/goit/gojavaonline2/practice/module2/AverageNumber.java
@@ -0,0 +1,8 @@
+package goit.gojavaonline2.practice.module2;
+
+public class AverageNumber {
+    public int average(int a, int b){
+        long sum = (long)a + (long)b;
+        return (int)(sum/2);
+    }
+}
---
+++ src/test/java/goit/gojavaonline2/practice/module2/AverageNumberTest.java
@@ -0,0 +1,40 @@
+package goit.gojavaonline2.practice.module2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class AverageNumberTest {
+    int a, b, expected;
+    AverageNumber sut = new AverageNumber();
+
+    public AverageNumberTest(int a, int b, int expected) {
+        this.a = a;
+        this.b = b;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+
+        return Arrays.asList(
+                new Object[]{4, 6, 5},
+                new Object[]{-4, -7, -5},
+                new Object[]{-4, 7, 1},
+                new Object[]{1073741824, 1073741824, 1073741824},
+                new Object[]{-1073741824, 1073741824, 0},
+                new Object[]{-1073741824, -1073741824, -1073741824}
+        );
+    }
+
+    @Test
+    public void testAverage() throws Exception {
+        int actual = sut.average(a, b);
+        Assert.assertEquals(expected, actual);
+    }
+}
