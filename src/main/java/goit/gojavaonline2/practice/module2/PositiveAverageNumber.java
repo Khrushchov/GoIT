19f276c5cd86af5258daa117b097edc8d2214e77
Copypaster <khruschiov@gmail.com>
2016-05-08 10:23:45 +0300

    module2: add task 2 - positive average number

---
+++ src/main/java/goit/gojavaonline2/practice/module2/PositiveAverageNumber.java
@@ -0,0 +1,8 @@
+package goit.gojavaonline2.practice.module2;
+
+public class PositiveAverageNumber {
+    public int average (int a, int b){
+        long sum = (long)a + (long)b;
+        return (int)(sum/2);
+    }
+}
---
+++ src/test/java/goit/gojavaonline2/practice/module2/PositiveAverageNumberTest.java
@@ -0,0 +1,38 @@
+package goit.gojavaonline2.practice.module2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class PositiveAverageNumberTest {
+    int a, b, expected;
+    PositiveAverageNumber sut = new PositiveAverageNumber();
+
+    public PositiveAverageNumberTest(int a, int b, int expected) {
+        this.a = a;
+        this.b = b;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+
+        return Arrays.asList(
+          new Object[]{2, 4, 3},
+          new Object[]{4, 7, 5},
+          new Object[]{0, 1, 0},
+          new Object[]{1073741824, 1073741824, 1073741824}
+        );
+    }
+
+    @Test
+    public void testAverage() throws Exception {
+        int actual = sut.average(a, b);
+        Assert.assertEquals(expected, actual);
+    }
+}
